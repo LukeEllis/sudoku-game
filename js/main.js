0819437f3c7138cdbbba152f1173fd0f634ffc42
Ellis <iky090@dca9047644a5.cof.ds.capitalone.com>
2017-08-23 11:42:39 -0400

    added level 3 board

--- js/main.js
+++ js/main.js
@@ -501,7 +501,7 @@ $('#levelButton2').on('click', function(){
         inputs.eq(39).val() == 9 &&
         inputs.eq(40).val() == 7 &&
         inputs.eq(41).val() == 2 &&
-        inputs.eq(42).val() == 4 &&
+        inputs.eq(42).val() == 4
     ){
       alert("Congratulations, you've beaten Level 2!!!");
       // Makes Level 2 button unclickable for future Sudoku Champion check
@@ -532,134 +532,92 @@ $('#levelButton3').on('click', function(){
     else if (i === 1){
       $(obj).text(2);
     }
-    else if (i === 3){
-      $(obj).text(7);
+    else if (i === 5){
+      $(obj).text(9);
     }
-    else if (i === 4){
-      $(obj).text(1);
-    }
-    else if (i === 8){
-      $(obj).text(3);
-    }
-    else if (i === 10){
+    else if (i === 6){
       $(obj).text(5);
     }
-    else if (i === 11){
-      $(obj).text(3);
+    else if (i === 12){
+      $(obj).text(7);
     }
-    else if (i === 13){
-      $(obj).text(8);
+    else if (i === 14){
+      $(obj).text(5);
     }
     else if (i === 15){
-      $(obj).text(7);
+      $(obj).text(8);
+    }
+    else if (i === 16){
+      $(obj).text(3);
+    }
+    else if (i === 19){
+      $(obj).text(5);
     }
     else if (i === 16){
       $(obj).text(1);
     }
-    else if (i === 20){
-      $(obj).text(6);
-    }
-    else if (i === 21){
-      $(obj).text(3);
-    }
-    else if (i === 22){
-      $(obj).text(5);
+    else if (i === 23){
+      $(obj).text(1);
     }
     else if (i === 24){
       $(obj).text(2);
     }
-    else if (i === 25){
-      $(obj).text(4);
+    else if (i === 27){
+      $(obj).text(9);
     }
-    else if (i === 30){
-      $(obj).text(5);
+    else if (i === 28){
+      $(obj).text(3);
     }
     else if (i === 31){
-      $(obj).text(9);
-    }
-    else if (i === 32){
-      $(obj).text(3);
-    }
-    else if (i === 33){
-      $(obj).text(6);
-    }
-    else if (i === 35){
-      $(obj).text(7);
-    }
-    else if (i === 36){
-      $(obj).text(5);
-    }
-    else if (i === 37){
-      $(obj).text(9);
-    }
-    else if (i === 38){
-      $(obj).text(2);
-    }
-    else if (i === 41){
-      $(obj).text(7);
-    }
-    else if (i === 43){
-      $(obj).text(8);
-    }
-    else if (i === 45){
-      $(obj).text(3);
-    }
-    else if (i === 46){
-      $(obj).text(6);
-    }
-    else if (i === 50){
-      $(obj).text(4);
-    }
-    else if (i === 51){
-      $(obj).text(9);
-    }
-    else if (i === 52){
-      $(obj).text(5);
-    }
-    else if (i === 54){
-      $(obj).text(6);
-    }
-    else if (i === 55){
-      $(obj).text(8);
-    }
-    else if (i === 56){
       $(obj).text(1);
     }
-    else if (i === 60){
-      $(obj).text(4);
+    else if (i === 37){
+      $(obj).text(6);
     }
-    else if (i === 62){
+    else if (i === 39){
+      $(obj).text(9);
+    }
+    else if (i === 42){
+      $(obj).text(1);
+    }
+    else if (i === 48){
       $(obj).text(2);
     }
-    else if (i === 65){
+    else if (i === 52){
+      $(obj).text(9);
+    }
+    else if (i === 53){
+      $(obj).text(3);
+    }
+    else if (i === 54){
+      $(obj).text(3);
+    }
+    else if (i === 56){
       $(obj).text(5);
     }
-    else if (i === 66){
-      $(obj).text(2);
+    else if (i === 61){
+      $(obj).text(8);
+    }
+    else if (i === 65){
+      $(obj).text(9);
     }
     else if (i === 67){
       $(obj).text(4);
     }
-    else if (i === 68){
-      $(obj).text(8);
-    }
     else if (i === 71){
-      $(obj).text(9);
+      $(obj).text(7);
     }
-    else if (i === 72){
+    else if (i === 74){
       $(obj).text(2);
     }
-    else if (i === 75){
-      $(obj).text(6);
-    }
-    else if (i === 77){
-      $(obj).text(1);
-    }
-    else if (i === 79){
+    else if (i === 76){
       $(obj).text(3);
     }
-    else if (i === 80){
+    else if (i === 77){
       $(obj).text(7);
+    }
+    else if (i === 80){
+      $(obj).text(6);
     }
     else{
       $(obj).append("<input class='puzzle-piece' type='text'>");
