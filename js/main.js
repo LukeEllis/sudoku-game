017aead6b2ad0949dc4e5f375291f02089edfc4f
Ellis <iky090@dca9047644a5.cof.ds.capitalone.com>
2017-08-23 17:05:51 -0400

    error checker complete for all levels

--- js/main.js
+++ js/main.js
@@ -346,7 +346,6 @@ $('#levelButton1').on('click', function(){
 /* VIII e. Level 1 Answers Button */
 //fills in all text input boxes with correct answers
 
-// var answers = [8, 6, 5 ..]
   $('#answers').on('click', function(){
     let inputs = $("input");
     inputs.eq(0).val('8') &&
@@ -592,10 +591,27 @@ $('#levelButton2').on('click', function(){
   });
 /* IX c. Level 2 Errors Button */
 
-  // $('#errors').on('click', function(){
-  //   //toggle error checker mode
-  //   //turn on red where answers are incorrect
-  // });
+  $('#errors').on('click', function(){
+    let inputs = $("input.puzzle-piece");
+    let answerArray = [
+      2, 1, 9, 6,
+      8, 3, 5, 1, 4,
+      4, 9, 1, 6, 5,
+      9, 7, 8, 5, 6,
+      2, 4, 9, 3, 5,
+      3, 6, 1, 7, 9,
+      4, 7, 6, 1, 9,
+      3, 1, 4, 7,
+      3, 9, 7, 2, 4
+    ];
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs.eq(i).val() == answerArray[i]) {
+        inputs.eq(i).css('background-color', 'white');
+      } else {
+        inputs.eq(i).css('background-color', 'red');
+      }
+    };
+  });
 /* IX d. Level 2 Hint Button */
 
   // $('#hint').on('click', function(){
@@ -836,10 +852,27 @@ $('#levelButton3').on('click', function(){
   });
 /* X c. Level 3 Errors Button */
 
-  // $('#errors').on('click', function(){
-  //   //toggle error checker mode
-  //   //turn on red where answers are incorrect
-  // });
+  $('#errors').on('click', function(){
+    let inputs = $("input.puzzle-piece");
+    let answerArray = [
+      8, 3, 6, 7, 1,
+      1, 9, 6, 2, 4,
+      7, 3, 4, 8, 6, 9,
+      4, 6, 8, 7, 2, 5,
+      2, 7, 5, 3, 4, 8,
+      5, 8, 1, 7, 4, 6,
+      7, 1, 9, 6, 4, 2,
+      6, 1, 8, 2, 3, 5,
+      8, 4, 5, 9, 1
+    ];
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs.eq(i).val() == answerArray[i]) {
+        inputs.eq(i).css('background-color', 'white');
+      } else {
+        inputs.eq(i).css('background-color', 'red');
+      }
+    };
+  });
 /* X d. Level 3 Hint Button */
 
   // $('#hint').on('click', function(){
@@ -1115,10 +1148,27 @@ $('#levelButton4').on('click', function(){
   });
 /* XI c. Level 4 Errors Button */
 
-  // $('#errors').on('click', function(){
-  //   //toggle error checker mode
-  //   //turn on red where answers are incorrect
-  // });
+  $('#errors').on('click', function(){
+    let inputs = $("input.puzzle-piece");
+    let answerArray = [
+      8, 6, 5, 9,
+      9, 4, 2, 6,
+      1, 7, 9, 8,
+      8, 1, 4, 2,
+      1, 6, 3, 4,
+      7, 8, 2, 1,
+      9, 3, 5, 7,
+      7, 3, 1, 6,
+      4, 9, 7, 8,
+    ];
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs.eq(i).val() == answerArray[i]) {
+        inputs.eq(i).css('background-color', 'white');
+      } else {
+        inputs.eq(i).css('background-color', 'red');
+      }
+    };
+  });
 /* XI d. Level 4 Hint Button */
 
   // $('#hint').on('click', function(){
@@ -1341,10 +1391,27 @@ $('#levelButton5').on('click', function(){
   });
 /* XII c. Level 5 Errors Button */
 
-  // $('#errors').on('click', function(){
-  //   //toggle error checker mode
-  //   //turn on red where answers are incorrect
-  // });
+  $('#errors').on('click', function(){
+    let inputs = $("input.puzzle-piece");
+    let answerArray = [
+      8, 6, 5, 9,
+      9, 4, 2, 6,
+      1, 7, 9, 8,
+      8, 1, 4, 2,
+      1, 6, 3, 4,
+      7, 8, 2, 1,
+      9, 3, 5, 7,
+      7, 3, 1, 6,
+      4, 9, 7, 8,
+    ];
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs.eq(i).val() == answerArray[i]) {
+        inputs.eq(i).css('background-color', 'white');
+      } else {
+        inputs.eq(i).css('background-color', 'red');
+      }
+    };
+  });
 /* XII d. Level 5 Hint Button */
 
   // $('#hint').on('click', function(){
